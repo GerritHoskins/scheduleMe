37139db37fdb3d8a67897bbdb603610099573595
GerritHoskins <gerrit.hoskins@gmail.com>
2020-04-19 16:52:28 +0200

    n5

--- functions/index.js
+++ functions/index.js
@@ -2,15 +2,25 @@
 
 const functions = require('firebase-functions');
 const app = require('express')();
+//const auth = require('./util/auth');
+const {
+    loginUser
+} = require('./APIs/users')
+
+// Users
+app.post('/login', loginUser);
 
 const {
     getAllTodos,
     postOneTodo,
-    deleteTodo
+    deleteTodo,
+    editTodo
 } = require('./APIs/todos')
 
 app.post('/todo', postOneTodo);
 app.get('/todos', getAllTodos);
 app.delete('/todo/:todoId', deleteTodo);
+app.put('/todo/:todoId', editTodo);
+
 
 exports.api = functions.https.onRequest(app);
