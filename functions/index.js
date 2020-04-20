d6f2abe240793b88962f247993c9265fe8ff2831
GerritHoskins <gerrit.hoskins@gmail.com>
2020-04-20 16:15:52 +0200

    JK6

--- functions/index.js
+++ functions/index.js
@@ -4,24 +4,26 @@ const functions = require('firebase-functions');
 const app = require('express')();
 //const auth = require('./util/auth');
 const {
-    loginUser
+    loginUser,
+    signUpUser
 } = require('./APIs/users')
 
 // Users
 app.post('/login', loginUser);
+app.post('/signup', signUpUser);
 
 const {
     getAllTodos,
     postOneTodo,
     deleteTodo,
     editTodo,
-    signUpUser
+   
 } = require('./APIs/todos')
 
 app.post('/todo', postOneTodo);
 app.get('/todos', getAllTodos);
 app.delete('/todo/:todoId', deleteTodo);
 app.put('/todo/:todoId', editTodo);
-app.post('/signup', signUpUser);
+
 
 exports.api = functions.https.onRequest(app);
