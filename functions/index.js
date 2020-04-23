014bd349c44a9054e29901f30d8bf083084a3f89
GerritHoskins <gerrit.hoskins@gmail.com>
2020-04-23 11:23:23 +0200

    l8

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
-//index.js
 const functions = require('firebase-functions');
 const app = require('express')();
 const auth = require('./util/auth');
-
+ 
 const {
+    getAllTodos,
+    getOneTodo,
+    postOneTodo,
+    deleteTodo,
+    editTodo
+} = require('./APIs/todos')
+
+const { 
     loginUser,
     signUpUser,
     uploadProfilePhoto,
@@ -11,25 +18,18 @@ const {
     updateUserDetails
 } = require('./APIs/users')
 
-// Users
-app.post('/login', loginUser);
-app.post('/signup', signUpUser);
-app.post('/user/image', auth, uploadProfilePhoto);
-app.get('/user', auth, getUserDetail);
-app.post('/user', auth, updateUserDetails);
-
-const {
-    getAllTodos,
-    getOneTodo,
-    postOneTodo,
-    deleteTodo,
-    editTodo   
-} = require('./APIs/todos')
-
+// Todos
 app.get('/todos', auth, getAllTodos);
 app.get('/todo/:todoId', auth, getOneTodo);
 app.post('/todo',auth, postOneTodo);
 app.delete('/todo/:todoId',auth, deleteTodo);
 app.put('/todo/:todoId',auth, editTodo);
+
+// Users
+app.post('/login', loginUser);
+app.post('/signup', signUpUser);
+app.post('/user/image', auth ,uploadProfilePhoto);
+app.post('/user', auth ,updateUserDetails);
+app.get('/user', auth, getUserDetail);
 
 exports.api = functions.https.onRequest(app);
--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,9 +1,11 @@
 //admin.js
-
 const admin = require('firebase-admin');
+const serviceAccount = require("../../dbtronics.json");
 
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://dbtronics-79c66.firebaseio.com"
+});
 
 const db = admin.firestore();
-
 module.exports = { admin, db };
